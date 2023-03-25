8ec39714fdae6fd0e900bb60cea285bd39872642
Miguel Aréjula Aisa <arejula10@gmail.com>
2023-03-25 17:29:32 +0100

    nand

--- fuentes/Mips_segmentado_IRQ_completar_2023.vhd
+++ fuentes/Mips_segmentado_IRQ_completar_2023.vhd
@@ -330,7 +330,7 @@ begin
 			port map (	Din => status_input, clk => clk, reset => reset, load => update_status, Dout => MIPS_status);
 	------------------------------------------------------------------------------------
 	-- Completar: falta la l�gica que detecta cu�ndo se va a procesaruna excepci�n: cuando se recibe una de las se�ales (IRQ, Data_abort y Undef) y las excepciones est�n habilitadas (MIPS_status(1)='1')
-	Exception_accepted <= (IRQ or Data_Abort or UNDEF) nand MIPS_status(1);
+	Exception_accepted <= (IRQ or Data_Abort or UNDEF) and not MIPS_status(1);
 	------------------------------------------------------------------------------------
 	-- Completar: falta la l�gica que gestiona update_status. Dise�adla.
 	update_status	<= Exception_accepted or (RTE_ID and valid_I_ID);
